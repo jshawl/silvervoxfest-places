a699d98d4f2098c54e013c4f4fcdc852b9ed0d00
Jesse Shawl <jesse@jesse.sh>
2026-04-29 11:00:34 -0400

    add teal social icons

---
+++ themes/2026/patterns/teal-social-icons.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Title: Teal Social Icons
+ * Slug: blank/teal-social-icons
+ * Categories: 
+ */
+?>
+<!-- wp:social-links {"openInNewTab":true} -->
+<ul class="wp-block-social-links"><!-- wp:social-link {"url":"https://www.facebook.com/silvervoxfest","service":"facebook","className":"has-teal-background-color"} /-->
+
+<!-- wp:social-link {"url":"https://www.linkedin.com/company/silvervox-fest/","service":"linkedin","className":"has-teal-background-color"} /-->
+
+<!-- wp:social-link {"url":"https://www.instagram.com/silvervoxfest/","service":"instagram","className":"has-teal-background-color"} /--></ul>
+<!-- /wp:social-links -->
