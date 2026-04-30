2a253ed85702ec1872a6e1173be8b77ecc1c90b0
Jesse Shawl <jesse@jesse.sh>
2026-04-30 08:04:48 -0400

    rename theme (#20)

--- themes/silvervoxfest-2026/functions.php
+++ themes/silvervoxfest-2026/functions.php
@@ -17,3 +17,19 @@ function sfmf_enqueue_dashicon()
     wp_enqueue_style('dashicons');
 }
 add_action('wp_enqueue_scripts', 'sfmf_enqueue_dashicon');
+
+
+add_action('init', 'sfmf_register_pattern_categories');
+
+function sfmf_register_pattern_categories()
+{
+    register_block_pattern_category('silvervoxfest-2026/custom', array(
+        'label'       => __('silvervoxfest 2026', 'silvervoxfest-2026'),
+        'description' => __('silvervoxfest 2026', 'silvervoxfest-2026')
+    ));
+}
+
+// TODO remove cache flush
+add_action('init', function () {
+    wp_get_theme()->delete_pattern_cache();
+});
---
+++ themes/silvervoxfest-2026/patterns/hero.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Title: Hero
+ * Slug: silvervoxfest-2026/hero
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:cover {"url":"http://localhost:8888/wp-content/uploads/2026/04/SL-121019-25870-26-1024x585.jpg","dimRatio":50,"customOverlayColor":"#b17066","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-cover is-light" style="padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><img class="wp-block-cover__image-background size-large" alt="" src="http://localhost:8888/wp-content/uploads/2026/04/SL-121019-25870-26-1024x585.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#b17066"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}}},"textColor":"goldenrod"} -->
+<p class="has-text-align-right has-goldenrod-color has-text-color has-link-color">creativity connects us</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"67px"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-text-align-left has-white-color has-text-color has-link-color" style="font-size:67px">Four days of film and music across one walkable city</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
+<p class="has-white-color has-text-color has-link-color">Films, live music, and conversation across ten walkable venues in historic downtown Frederick. Independent voices. The artists keep the door.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons -->
+<div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"pink"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background wp-element-button">Reserve Your Badge</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div></div>
+<!-- /wp:cover -->
+
+<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"goldenrod","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-goldenrod-background-color has-background" style="margin-top:0;margin-bottom:0;padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}}},"textColor":"pink"} -->
+<p class="has-pink-color has-text-color has-link-color"><strong>now announcing</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:latest-posts {"postsToShow":2,"postLayout":"grid","style":{"layout":{"selfStretch":"fill","flexSize":null}}} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
