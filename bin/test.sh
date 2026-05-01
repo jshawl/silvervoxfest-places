3a47cd83ecf90dfd12cf375ba3c6f58b5c1a0d2e
Jesse Shawl <jesse@jesse.sh>
2026-05-01 05:59:30 -0400

    run tests in pre-push hook

--- bin/test.sh
+++ bin/test.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
+faketty() {
+    python3 -c "
+import pty, os
+status = pty.spawn(['/bin/bash', '-c', '$*'])
+exit(os.waitstatus_to_exitcode(status))
+"
+}
+
 for plugin in plugins/*; do
     cd $plugin
     npm test
     cd -
-    wp-env run tests-cli --env-cwd=wp-content/$plugin vendor/bin/phpunit --coverage-html coverage/php
+    faketty wp-env run tests-cli --env-cwd=wp-content/$plugin vendor/bin/phpunit --coverage-html coverage/php
 done
