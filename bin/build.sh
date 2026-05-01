8e4881f2d52ca64603ff5ea9cd7ad041b1a9f996
Jesse Shawl <jesse@jesse.sh>
2026-04-30 20:20:04 -0400

    zip theme dir

--- bin/build.sh
+++ bin/build.sh
@@ -3,16 +3,31 @@
 rm -rf build/*
 mkdir -p build/
 
-for plugin in plugins/*; do
-    base=$(basename "$plugin")
-    file="$plugin/$base.php"
-    sha=$(git log --format="%h" -n 1 -- "$plugin")
+build_item() {
+    local dir="$1"
+    local file="$2"
+    local zip_contents=("${@:3}")
 
-    old_ver=$(grep -oE '[0-9]+\.[0-9]+' "$file" | head -1)
-    new_ver="1.0+$sha"
+    local base
+    base=$(basename "$dir")
+    local sha
+    sha=$(git log --format="%h" -n 1 -- "$dir")
+    local old_ver
+    old_ver=$(sed -nE 's/( \* )?Version: ([0-9]+\.[0-9]+).*/\2/p' "$file" | head -1)
+    local new_ver="1.0+$sha"
 
     echo "Building $base $new_ver"
     perl -pi -e "s/Version: $old_ver/Version: $new_ver/" "$file"
-    (cd plugins && zip -r "../build/$base.zip" "$base/includes/" "$base/$base.php" > /dev/null) || true
+    (cd "$(dirname "$dir")" && zip -r "../build/$base.zip" "${zip_contents[@]}" > /dev/null) || true
     perl -pi -e "s/Version: \Q$new_ver\E/Version: $old_ver/" "$file"
+}
+
+for plugin in plugins/*; do
+    base=$(basename "$plugin")
+    build_item "$plugin" "$plugin/$base.php" "$base/includes/" "$base/$base.php"
+done
+
+for theme in themes/*; do
+    base=$(basename "$theme")
+    build_item "$theme" "$theme/style.css" "$base/"
 done
