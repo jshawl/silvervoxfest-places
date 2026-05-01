5d7e46f82dde525e1ebc02efe4b64dadb5c25ec2
Jesse Shawl <jesse@jesse.sh>
2026-04-30 20:43:38 -0400

    encode timestamp and sha in filename

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -eou pipefail
+
 rm -rf build/*
 mkdir -p build/
 
@@ -7,18 +9,16 @@ build_item() {
     local dir="$1"
     local file="$2"
     local zip_contents=("${@:3}")
+    local base=$(basename "$dir")
+    local sha d
+    read -r sha d < <(git log --format="%h %ai" -n 1 -- "$dir")
+    local timestamp="${d:5:2}${d:8:2}${d:11:2}"
+    local old_ver=$(sed -nE 's/( \* )?Version: ([0-9]+\.[0-9]+).*/\2/p' "$file" | head -1)
+    local new_ver="1.0+$timestamp-$sha"
 
-    local base
-    base=$(basename "$dir")
-    local sha
-    sha=$(git log --format="%h" -n 1 -- "$dir")
-    local old_ver
-    old_ver=$(sed -nE 's/( \* )?Version: ([0-9]+\.[0-9]+).*/\2/p' "$file" | head -1)
-    local new_ver="1.0+$sha"
-
-    echo "Building $base $new_ver"
+    echo "Building $base v$new_ver"
     perl -pi -e "s/Version: $old_ver/Version: $new_ver/" "$file"
-    (cd "$(dirname "$dir")" && zip -r "../build/$base.zip" "${zip_contents[@]}" > /dev/null) || true
+    (cd "$(dirname "$dir")" && zip -r "../build/$base-${new_ver//+/-}.zip" "${zip_contents[@]}" > /dev/null)
     perl -pi -e "s/Version: \Q$new_ver\E/Version: $old_ver/" "$file"
 }
 
