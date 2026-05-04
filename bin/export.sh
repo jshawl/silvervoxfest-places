9fddb84f1e7120649a8a85e7a8d734109c291fd2
Jesse Shawl <jesse@jesse.sh>
2026-05-03 20:23:39 -0400

    version db export (#23)

--- bin/export.sh
+++ bin/export.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-wp-env run cli wp db export - > ./dump.sql
+now=$(date +"%y%m%d-%H%M")
+wp-env run cli wp db export - > ./db/$now.sql
+rm -f ./db/latest.sql
+cp ./db/$now.sql ./db/latest.sql
