223bd2b9042d4e3cf90208f2e993ed9bee3dc550
Jesse Shawl <jesse@jesse.sh>
2026-05-06 06:42:58 -0400

    add fonts

--- themes/silvervoxfest-2026/patterns/hero.php
+++ themes/silvervoxfest-2026/patterns/hero.php
@@ -5,7 +5,7 @@
  * Categories: silvervoxfest-2026/custom
  */
 ?>
-<!-- wp:cover {"url":"http://localhost:8888/wp-content/uploads/2026/04/SL-121019-25870-26-1024x585.jpg","dimRatio":50,"customOverlayColor":"#b17066","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<!-- wp:cover {"url":"http://localhost:8888/wp-content/themes/silvervoxfest-2026/assets/img/hero.png","dimRatio":50,"customOverlayColor":"#b17066","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
 <div class="wp-block-cover is-light" style="padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><img class="wp-block-cover__image-background size-large" alt="" src="http://localhost:8888/wp-content/uploads/2026/04/SL-121019-25870-26-1024x585.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#b17066"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}}},"textColor":"goldenrod"} -->
 <p class="has-text-align-right has-goldenrod-color has-text-color has-link-color">creativity connects us</p>
 <!-- /wp:paragraph -->
