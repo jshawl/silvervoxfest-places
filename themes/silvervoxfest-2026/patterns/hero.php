f8cdd364b12b87a62fae0bb751799f0b5b94f615
Jesse Shawl <jesse@jesse.sh>
2026-05-07 05:55:32 -0400

    improve header

--- themes/silvervoxfest-2026/patterns/hero.php
+++ themes/silvervoxfest-2026/patterns/hero.php
@@ -5,14 +5,14 @@
  * Categories: silvervoxfest-2026/custom
  */
 ?>
-<!-- wp:cover {"url":"http://localhost:8888/wp-content/themes/silvervoxfest-2026/assets/img/hero.png","dimRatio":50,"customOverlayColor":"#b17066","isUserOverlayColor":false,"isDark":false,"sizeSlug":"large","style":{"spacing":{"padding":{"top":"0","bottom":"0","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
-<div class="wp-block-cover is-light" style="padding-top:0;padding-right:var(--wp--preset--spacing--50);padding-bottom:0;padding-left:var(--wp--preset--spacing--50)"><img class="wp-block-cover__image-background size-large" alt="" src="http://localhost:8888/wp-content/uploads/2026/04/SL-121019-25870-26-1024x585.jpg" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim" style="background-color:#b17066"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}}},"textColor":"goldenrod"} -->
-<p class="has-text-align-right has-goldenrod-color has-text-color has-link-color">creativity connects us</p>
+<!-- wp:cover {"url":"http://localhost:8888/wp-content/uploads/2026/05/earache_dynamic_abstract_composition_flowing_shapes_represent_afa6bff7-f579-43f0-b188-dac032fb42d3_0-1-1-1024x476.png","dimRatio":10,"isUserOverlayColor":true,"sizeSlug":"large","layout":{"type":"constrained"}} -->
+<div class="wp-block-cover"><img class="wp-block-cover__image-background  size-large" alt="" src="http://localhost:8888/wp-content/uploads/2026/05/earache_dynamic_abstract_composition_flowing_shapes_represent_afa6bff7-f579-43f0-b188-dac032fb42d3_0-1-1-1024x476.png" data-object-fit="cover"/><span aria-hidden="true" class="wp-block-cover__background has-background-dim-10 has-background-dim"></span><div class="wp-block-cover__inner-container"><!-- wp:paragraph {"align":"right","style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}}},"textColor":"goldenrod","fontFamily":"bebas-neue"} -->
+<p class="has-text-align-right has-goldenrod-color has-text-color has-link-color has-bebas-neue-font-family">★ creativity connects us</p>
 <!-- /wp:paragraph -->
 
-<!-- wp:paragraph {"align":"left","placeholder":"Write title…","style":{"typography":{"fontSize":"67px"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
-<p class="has-text-align-left has-white-color has-text-color has-link-color" style="font-size:67px">Four days of film and music across one walkable city</p>
-<!-- /wp:paragraph -->
+<!-- wp:heading {"style":{"typography":{"fontSize":"86px","lineHeight":"1"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-white-color has-text-color has-link-color has-bebas-neue-font-family" style="font-size:86px;line-height:1">4 days of Film and music<br>across one walkable City</h2>
+<!-- /wp:heading -->
 
 <!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white"} -->
 <p class="has-white-color has-text-color has-link-color">Films, live music, and conversation across ten walkable venues in historic downtown Frederick. Independent voices. The artists keep the door.</p>
@@ -20,17 +20,7 @@
 
 <!-- wp:buttons -->
 <div class="wp-block-buttons"><!-- wp:button {"backgroundColor":"pink"} -->
-<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background wp-element-button">Reserve Your Badge</a></div>
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background wp-element-button">Reserve your Badge →</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div></div>
 <!-- /wp:cover -->
-
-<!-- wp:group {"style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}}},"backgroundColor":"goldenrod","layout":{"type":"constrained"}} -->
-<div class="wp-block-group has-goldenrod-background-color has-background" style="margin-top:0;margin-bottom:0;padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
-<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}}},"textColor":"pink"} -->
-<p class="has-pink-color has-text-color has-link-color"><strong>now announcing</strong></p>
-<!-- /wp:paragraph -->
-
-<!-- wp:latest-posts {"postsToShow":2,"postLayout":"grid","style":{"layout":{"selfStretch":"fill","flexSize":null}}} /--></div>
-<!-- /wp:group --></div>
-<!-- /wp:group -->
