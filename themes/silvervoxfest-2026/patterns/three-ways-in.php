5b618d026b9df957710f05fe55524d6ddbc99f18
Jesse Shawl <jesse@jesse.sh>
2026-05-09 17:08:15 -0400

    add three ways in

---
+++ themes/silvervoxfest-2026/patterns/three-ways-in.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Title: Three Ways In
+ * Slug: silvervoxfest-2026/three-ways-in
+ * Categories: 
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"typography":{"fontSize":"36px"}},"fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-bebas-neue-font-family" style="font-size:36px">Three Ways In</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"fontFamily":"dm-sans"} -->
+<p class="has-dm-sans-font-family">ALL PASSES →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"align-items-flex-start ticket-images","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left","orientation":"horizontal"}} -->
+<div class="wp-block-group align-items-flex-start ticket-images"><!-- wp:group {"metadata":{"categories":["silvervoxfest-2026/custom"],"patternName":"silvervoxfest-2026/gradient-border","name":"ticket-description"},"className":"gradient-border","layout":{"type":"default"}} -->
+<div class="wp-block-group gradient-border"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="/wp-content/themes/silvervoxfest-2026/assets/img/lanyard.png" alt="" style="object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","fontSize":"large","fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-text-align-center has-bebas-neue-font-family has-large-font-size">All access. All four days</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Every film, every show, every talk. Opening and closing parties. Artist lounge. Priority for sold-out screenings.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"pink"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background has-text-align-center wp-element-button">On Sale Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["silvervoxfest-2026/custom"],"patternName":"silvervoxfest-2026/gradient-border","name":"ticket-description"},"className":"gradient-border","layout":{"type":"default"}} -->
+<div class="wp-block-group gradient-border"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="/wp-content/themes/silvervoxfest-2026/assets/img/lanyard2.png" alt="" style="object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","fontSize":"large","fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-text-align-center has-bebas-neue-font-family has-large-font-size">Friday or Saturday</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Every film, every show, every talk. Opening and closing parties. Artist lounge. Priority for sold-out screenings.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"pink"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background has-text-align-center wp-element-button">On Sale Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"categories":["silvervoxfest-2026/custom"],"patternName":"silvervoxfest-2026/gradient-border","name":"ticket-description"},"className":"gradient-border","layout":{"type":"default"}} -->
+<div class="wp-block-group gradient-border"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"scale":"cover","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large"><img src="/wp-content/themes/silvervoxfest-2026/assets/img/ticket.png" alt="" style="object-fit:cover"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","fontSize":"large","fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-text-align-center has-bebas-neue-font-family has-large-font-size">Pick what you want to see</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Every film, every show, every talk. Opening and closing parties. Artist lounge. Priority for sold-out screenings.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"pink"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background has-text-align-center wp-element-button">On Sale Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
---
+++ themes/silvervoxfest-2026/patterns/ticket-description.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Title: ticket-description
+ * Slug: silvervoxfest-2026/gradient-border
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"className":"gradient-border","layout":{"type":"default"}} -->
+<div class="wp-block-group gradient-border"><!-- wp:group {"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:image {"width":"250px","sizeSlug":"large","linkDestination":"none"} -->
+<figure class="wp-block-image size-large is-resized"><img src="/wp-content/themes/silvervoxfest-2026/assets/img/lanyard.png" alt="" style="width:100%"/></figure>
+<!-- /wp:image -->
+
+<!-- wp:heading {"textAlign":"center","fontSize":"large","fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-text-align-center has-bebas-neue-font-family has-large-font-size">All access. All four days</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"align":"center","style":{"spacing":{"margin":{"top":"0","bottom":"0"}}}} -->
+<p class="has-text-align-center" style="margin-top:0;margin-bottom:0">Every film, every show, every talk. Opening and closing parties. Artist lounge. Priority for sold-out screenings.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"center"}} -->
+<div class="wp-block-buttons"><!-- wp:button {"textAlign":"center","backgroundColor":"pink"} -->
+<div class="wp-block-button"><a class="wp-block-button__link has-pink-background-color has-background has-text-align-center wp-element-button">On Sale Now</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
