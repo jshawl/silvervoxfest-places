5467b48eb06a9763b3f404faec881699e80c8ac8
Jesse Shawl <jesse@jesse.sh>
2026-05-09 16:05:46 -0400

    add program tracks

---
+++ themes/silvervoxfest-2026/patterns/program-tracks.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Title: Program Tracks
+ * Slug: silvervoxfest-2026/program-tracks
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"border":{"top":{"style":"none","width":"0px"},"right":{"style":"none","width":"0px"},"bottom":{"width":"1px","color":"var:preset|color|goldenrod"},"left":{"style":"none","width":"0px"}}},"layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group" style="border-top-style:none;border-top-width:0px;border-right-style:none;border-right-width:0px;border-bottom-color:var(--wp--preset--color--goldenrod);border-bottom-width:1px;border-left-style:none;border-left-width:0px"><!-- wp:heading {"style":{"typography":{"fontSize":"42px"}},"fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-bebas-neue-font-family" style="font-size:42px">Program Tracks</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"typography":{"fontSize":"16px"}},"textColor":"goldenrod"} -->
+<p class="has-goldenrod-color has-text-color has-link-color" style="font-size:16px"><em>Three streams. One Pass</em></p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"program-tracks","layout":{"type":"flex","flexWrap":"nowrap","justifyContent":"left"}} -->
+<div class="wp-block-group program-tracks"><!-- wp:group {"metadata":{"patternName":"silvervoxfest-2026/track","name":"Track"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"0"}},"backgroundColor":"blue","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-blue-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"typography":{"fontSize":"40px"}},"textColor":"goldenrod","fontFamily":"bebas-neue"} -->
+<p class="has-goldenrod-color has-text-color has-link-color has-bebas-neue-font-family" style="font-size:40px">01 / Film</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"120px","lineHeight":"1"},"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|40"}}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--40);font-size:120px;line-height:1">Cinema.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>100+ films · juried + audience awards</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Premieres, documentaries, midnight selections, and short film blocks. Jury includes Eduardo Sánchez. Audience awards across categories.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"patternName":"silvervoxfest-2026/track","name":"Track"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"0"}},"backgroundColor":"pink","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-pink-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"typography":{"fontSize":"40px"}},"textColor":"goldenrod","fontFamily":"bebas-neue"} -->
+<p class="has-goldenrod-color has-text-color has-link-color has-bebas-neue-font-family" style="font-size:40px">02 / Music</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"120px","lineHeight":"1"},"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|40"}}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--40);font-size:120px;line-height:1">Live.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>9 stages · 4 nights of programmed shows</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Music across Sky Stage, Weinberg, New Spire, and pop-up rooms downtown. Curated by SilverVox music programming.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"metadata":{"patternName":"silvervoxfest-2026/track","name":"Track"},"style":{"spacing":{"padding":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50","left":"var:preset|spacing|50","right":"var:preset|spacing|50"},"blockGap":"0"}},"backgroundColor":"teal","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-teal-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"typography":{"fontSize":"40px"}},"textColor":"goldenrod","fontFamily":"bebas-neue"} -->
+<p class="has-goldenrod-color has-text-color has-link-color has-bebas-neue-font-family" style="font-size:40px">03 / Talks</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"120px","lineHeight":"1"},"spacing":{"margin":{"top":"var:preset|spacing|80","bottom":"var:preset|spacing|40"}}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="margin-top:var(--wp--preset--spacing--80);margin-bottom:var(--wp--preset--spacing--40);font-size:120px;line-height:1">Ideas.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><strong>Free with any festival pass</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Filmmaker panels, craft conversations, and afternoon sessions on the work behind the work.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
