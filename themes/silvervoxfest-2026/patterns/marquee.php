8bd4308a071a72add7bb5cd63366b82b8374446a
Jesse Shawl <jesse@jesse.sh>
2026-05-09 06:41:32 -0400

    add marquee

---
+++ themes/silvervoxfest-2026/patterns/marquee.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Title: Marquee
+ * Slug: silvervoxfest-2026/marquee
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"className":"sfmf-marquee","style":{"spacing":{"margin":{"top":"0","bottom":"0"},"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50"}},"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"backgroundColor":"goldenrod","textColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group sfmf-marquee has-black-color has-goldenrod-background-color has-text-color has-background has-link-color" style="margin-top:0;margin-bottom:0;padding-right:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap","orientation":"horizontal","justifyContent":"space-between"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}}},"textColor":"pink","fontFamily":"bebas-neue"} -->
+<p class="has-pink-color has-text-color has-link-color has-bebas-neue-font-family"><strong>now announcing</strong></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:latest-posts {"postsToShow":2,"postLayout":"grid","style":{"layout":{"selfStretch":"fill","flexSize":null},"typography":{"textTransform":"uppercase"},"spacing":{"padding":{"top":"var:preset|spacing|30","bottom":"var:preset|spacing|30"}}},"fontFamily":"bebas-neue"} /--></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
