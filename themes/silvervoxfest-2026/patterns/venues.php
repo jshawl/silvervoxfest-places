83d825feae07c58457334cb067fd98333ac1dca8
Jesse Shawl <jesse@jesse.sh>
2026-05-10 16:23:55 -0400

    add venues

---
+++ themes/silvervoxfest-2026/patterns/venues.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Title: Venues
+ * Slug: silvervoxfest-2026/venues
+ * Categories: 
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"goldenrod","fontSize":"small"} -->
+<p class="has-goldenrod-color has-text-color has-link-color has-small-font-size" style="margin-top:0;margin-bottom:0">VENUES</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"0","bottom":"0"}},"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey","fontFamily":"georgia"} -->
+<p class="has-grey-color has-text-color has-link-color has-georgia-font-family" style="margin-top:0;margin-bottom:0"><br>Weinberg Center for the Arts  ·  New Spire Arts  ·  Maryland Ensemble Theatre  ·  Delaplaine Arts Center  ·  Frederick Center  ·  YMCA Ausherman Arts Center  ·  Sky Stage  ·  Warehouse Cinemas</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"className":"max-width-300","style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}},"border":{"bottom":{"width":"1px"}},"layout":{"selfStretch":"fixed","flexSize":"50%"}},"textColor":"pink","fontSize":"small","fontFamily":"dm-sans"} -->
+<p class="max-width-300 has-pink-color has-text-color has-link-color has-dm-sans-font-family has-small-font-size" style="border-bottom-width:1px">VIEW THE FESTIVAL MAP →</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
