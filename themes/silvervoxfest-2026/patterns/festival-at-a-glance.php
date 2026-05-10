ad29e6b07346fd6c47ed9eb24c4f300fa394ec9e
Jesse Shawl <jesse@jesse.sh>
2026-05-10 06:11:28 -0400

    add festival at a glance

---
+++ themes/silvervoxfest-2026/patterns/festival-at-a-glance.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Title: Festival at a glance
+ * Slug: silvervoxfest-2026/festival-at-a-glance
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|goldenrod","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--goldenrod);border-bottom-width:1px"><!-- wp:heading {"fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-bebas-neue-font-family">The festival at a glance</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group align-items-stretch"><!-- wp:group {"className":"flex-1-1 s-border-none","style":{"border":{"right":{"color":"var:preset|color|goldenrod","width":"1px"},"top":[],"bottom":[],"left":[]},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group flex-1-1 s-border-none" style="border-right-color:var(--wp--preset--color--goldenrod);border-right-width:1px;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|blue"}}}},"textColor":"blue","fontFamily":"bebas-neue"} -->
+<p class="has-blue-color has-text-color has-link-color has-bebas-neue-font-family">Film</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"96px"}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="font-size:96px">100+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey"} -->
+<p class="has-grey-color has-text-color has-link-color">Narrative, doc, midnight, music video, experimental — shorts and features.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex-1-1 s-border-none","style":{"border":{"right":{"color":"var:preset|color|goldenrod","width":"1px"},"top":[],"bottom":[],"left":[]},"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group flex-1-1 s-border-none" style="border-right-color:var(--wp--preset--color--goldenrod);border-right-width:1px;padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|teal"}}}},"textColor":"teal","fontFamily":"bebas-neue"} -->
+<p class="has-teal-color has-text-color has-link-color has-bebas-neue-font-family">Venues</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"96px"}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="font-size:96px">10+</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey"} -->
+<p class="has-grey-color has-text-color has-link-color">From Weinberg Center to Sky Stage. Walkable, all of it.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"className":"flex-1-1 s-border-none","style":{"spacing":{"padding":{"right":"var:preset|spacing|60","left":"var:preset|spacing|60"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group flex-1-1 s-border-none" style="padding-right:var(--wp--preset--spacing--60);padding-left:var(--wp--preset--spacing--60)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}}},"textColor":"pink","fontFamily":"bebas-neue"} -->
+<p class="has-pink-color has-text-color has-link-color has-bebas-neue-font-family">Days</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"typography":{"fontSize":"96px"}},"fontFamily":"bebas-neue"} -->
+<p class="has-bebas-neue-font-family" style="font-size:96px">4</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey"} -->
+<p class="has-grey-color has-text-color has-link-color">Thursday opening to Sunday awards. Programmed end to end.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
