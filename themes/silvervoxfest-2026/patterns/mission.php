027ea504489cb23427cf5ef84fdf8622dd4e947d
Jesse Shawl <jesse@jesse.sh>
2026-05-10 06:44:41 -0400

    add mission

---
+++ themes/silvervoxfest-2026/patterns/mission.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Title: Mission
+ * Slug: silvervoxfest-2026/mission
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"className":"sfmf-mission","style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"goldenrod","layout":{"type":"constrained"}} -->
+<div class="wp-block-group sfmf-mission has-goldenrod-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"className":"align-items-flex-start","layout":{"type":"flex","flexWrap":"wrap"}} -->
+<div class="wp-block-group align-items-flex-start"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"className":"prefix-star","style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black","fontFamily":"bebas-neue"} -->
+<p class="prefix-star has-black-color has-text-color has-link-color has-bebas-neue-font-family">The Mission</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|black"}}}},"textColor":"black"} -->
+<p class="has-black-color has-text-color has-link-color">SilverVox is a nonprofit connecting artists with audiences across film, music, and theater. Through mentorship, workshops, and flagship events, we support creators from first idea to final stage.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group -->
+
+<!-- wp:image {"sizeSlug":"large"} -->
+<figure class="wp-block-image size-large"><img src="/wp-content/themes/silvervoxfest-2026/assets/img/silvervox-logo-color.png" alt=""/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
