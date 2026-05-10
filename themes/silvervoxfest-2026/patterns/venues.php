7707727a36263f15486b9d58f29858037c682d81
Jesse Shawl <jesse@jesse.sh>
2026-05-10 16:31:58 -0400

    fix alignment and background

--- themes/silvervoxfest-2026/patterns/venues.php
+++ themes/silvervoxfest-2026/patterns/venues.php
@@ -5,8 +5,10 @@
  * Categories: 
  */
 ?>
-<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained","justifyContent":"left"}} -->
-<div class="wp-block-group" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"goldenrod","fontSize":"small"} -->
+<!-- wp:group {"metadata":{"categories":["silvervoxfest-2026/custom"],"patternName":"silvervoxfest-2026/venues","name":"Venues"},"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"black","layout":{"type":"constrained","justifyContent":"center"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:group {"layout":{"type":"constrained","justifyContent":"left"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"elements":{"link":{"color":{"text":"var:preset|color|goldenrod"}}},"spacing":{"margin":{"top":"0","bottom":"0"}}},"textColor":"goldenrod","fontSize":"small"} -->
 <p class="has-goldenrod-color has-text-color has-link-color has-small-font-size" style="margin-top:0;margin-bottom:0">VENUES</p>
 <!-- /wp:paragraph -->
 
@@ -17,4 +19,6 @@
 <!-- wp:paragraph {"className":"max-width-300","style":{"elements":{"link":{"color":{"text":"var:preset|color|pink"}}},"border":{"bottom":{"width":"1px"}},"layout":{"selfStretch":"fixed","flexSize":"50%"}},"textColor":"pink","fontSize":"small","fontFamily":"dm-sans"} -->
 <p class="max-width-300 has-pink-color has-text-color has-link-color has-dm-sans-font-family has-small-font-size" style="border-bottom-width:1px">VIEW THE FESTIVAL MAP →</p>
 <!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group --></div>
 <!-- /wp:group -->
