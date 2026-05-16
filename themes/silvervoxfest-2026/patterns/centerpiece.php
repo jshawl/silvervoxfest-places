14e706ed01fe9571ff2667bb0746e3a2abcb8310
Jesse Shawl <jesse@jesse.sh>
2026-05-16 05:44:49 -0400

    add centerpiece

---
+++ themes/silvervoxfest-2026/patterns/centerpiece.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Title: Centerpiece
+ * Slug: silvervoxfest-2026/centerpiece
+ * Categories: silvervoxfest-2026/custom
+ */
+?>
+<!-- wp:group {"style":{"spacing":{"padding":{"right":"var:preset|spacing|50","left":"var:preset|spacing|50","top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"backgroundColor":"black","layout":{"type":"constrained"}} -->
+<div class="wp-block-group has-black-background-color has-background" style="padding-top:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--50);padding-bottom:var(--wp--preset--spacing--50);padding-left:var(--wp--preset--spacing--50)"><!-- wp:group {"style":{"border":{"bottom":{"color":"var:preset|color|goldenrod","width":"1px"},"top":[],"right":[],"left":[]}},"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group" style="border-bottom-color:var(--wp--preset--color--goldenrod);border-bottom-width:1px"><!-- wp:heading {"fontFamily":"bebas-neue"} -->
+<h2 class="wp-block-heading has-bebas-neue-font-family">Centerpiece</h2>
+<!-- /wp:heading --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:query {"queryId":31,"query":{"perPage":1,"pages":0,"offset":0,"postType":"post","order":"desc","orderBy":"date","author":"","search":"","exclude":[],"sticky":"","inherit":false},"metadata":{"categories":["posts"],"patternName":"core/query-medium-posts","name":"Image at left"}} -->
+<div class="wp-block-query"><!-- wp:post-template -->
+<!-- wp:columns {"align":"wide"} -->
+<div class="wp-block-columns alignwide"><!-- wp:column {"width":"66.66%"} -->
+<div class="wp-block-column" style="flex-basis:66.66%"><!-- wp:post-featured-image {"isLink":true} /--></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":"33.33%"} -->
+<div class="wp-block-column" style="flex-basis:33.33%"><!-- wp:post-title {"isLink":true,"style":{"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey","fontFamily":"bebas-neue"} /-->
+
+<!-- wp:post-excerpt {"moreText":"Read More","style":{"typography":{"fontSize":"10px"},"elements":{"link":{"color":{"text":"var:preset|color|grey"}}}},"textColor":"grey"} /--></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns -->
+<!-- /wp:post-template --></div>
+<!-- /wp:query --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
