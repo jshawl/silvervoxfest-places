0321601ab23aecd8cd793833b6dd576760639015
Jesse Shawl <jesse@jesse.sh>
2026-05-16 13:12:18 -0400

    add spotify

--- themes/silvervoxfest-2026/patterns/teal-social-icons.php
+++ themes/silvervoxfest-2026/patterns/teal-social-icons.php
@@ -11,5 +11,6 @@
 
 <!-- wp:social-link {"url":"https://www.linkedin.com/company/silvervox-fest/","service":"linkedin","className":"has-teal-background-color"} /-->
 
-<!-- wp:social-link {"url":"https://www.instagram.com/silvervoxfest/","service":"instagram","className":"has-teal-background-color"} /--></ul>
+<!-- wp:social-link {"url":"https://www.instagram.com/silvervoxfest/","service":"instagram","className":"has-teal-background-color"} /-->
+<!-- wp:social-link {"url":"https://open.spotify.com/show/2T1CyxpzPRoY1Lu496rSYy?si=dbd50d54476849ee","service":"spotify","className":"has-teal-background-color"} /--></ul>
 <!-- /wp:social-links -->
